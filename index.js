e3716ed5acf8ebdd4513f91c8255a8ebb2e66386
phillipnzau <nzauonline78@gmail.com>
2023-03-22 12:30:47 +0300

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution in this file!
+const employee = {
+    name: "Phillip Nzau",
+    streetAddress: "Nairobi, kenya",
+
+}
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    const newObj = {...obj};
+    newObj[key] = value;
+    return newObj;    
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    obj[key] = value;
+    return obj;    
+}
+function deleteFromEmployeeByKey(obj, key2) {
+    const newObj = {...obj};
+    for (const key in newObj) {
+        if(key == key2) {
+            delete newObj[key]
+        }
+    }
+    return newObj;
+  
+}
+function destructivelyDeleteFromEmployeeByKey(obj, key2) {
+    for (const key in obj) {
+        if(key == key2) {
+            delete obj[key]
+        }
+    }
+    return obj;
+  
+}
